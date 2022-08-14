b27cf49f27f5acc151a6034fa93631478f669c60
matiasconsiglio <macastro1@uc.cl>
2022-08-14 10:12:00 +0000

    add category model

--- products/admin.py
+++ products/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Product
+from .models import Product, Category
 
 # Register your models here.
 
@@ -7,10 +7,17 @@ class ProductAdmin(admin.ModelAdmin):
     list_display = (
         'sku',
         'name',
+        'category',
         'price',
         'rating',
         'image',
     )
     ordering = ('sku',)
 
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+    )
+
 admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
--- products/models.py
+++ products/models.py
@@ -2,8 +2,19 @@ from django.db import models
 
 # Create your models here.
 
+class Category(models.Model):
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+        
+    name = models.CharField(max_length=254)
+
+    def __str__(self):
+        return self.name
+
 class Product(models.Model):
 
+    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
     sku = models.CharField(max_length=254, null=True, blank=True)
     name = models.CharField(max_length=254)
     description = models.TextField()
