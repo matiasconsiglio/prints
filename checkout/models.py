68a16ccac8d25affdb3c39134dcf7b45c9859649
matiasconsiglio <macastro1@uc.cl>
2022-08-30 18:20:55 +0000

    fix inline model, add checkout html

--- checkout/models.py
+++ checkout/models.py
@@ -31,7 +31,7 @@ class Order(models.Model):
         Update grand total each time a line item is added,
         accounting for delivery costs.
         """
-        self.order_total = self.lineitems.aggregate(Sum('lineitem_total'))['lineitem_total__sum']
+        self.order_total = self.lineitems.aggregate(Sum('lineitem_total'))['lineitem_total__sum'] or 0
         if self.order_total < settings.FREE_DELIVERY_THRESHOLD:
             self.delivery_cost = self.order_total * settings.STANDARD_DELIVERY_PERCENTAGE / 100
         else:
@@ -54,17 +54,22 @@ class Order(models.Model):
 
 class OrderLineItem(models.Model):
     order = models.ForeignKey(Order, null=False, blank=False, on_delete=models.CASCADE, related_name='lineitems')
-    product = models.ForeignKey(ProductSpec, null=False, blank=False, on_delete=models.CASCADE)
+    product_id = models.CharField(max_length=10, null=True, blank=True)
+    product_name = models.CharField(max_length=20, null=True, blank=True)
     quantity = models.IntegerField(null=False, blank=False, default=0)
     lineitem_total = models.DecimalField(max_digits=6, decimal_places=2, null=False, blank=False, editable=False)
+    price = models.PositiveIntegerField(null=True, default=0)
+    size = models.CharField(max_length=20, null=True, blank=True)
+    paper = models.CharField(max_length=100, null=True, blank=True)
+
 
     def save(self, *args, **kwargs):
         """
         Override the original save method to set the lineitem total
         and update the order total.
         """
-        self.lineitem_total = self.product.price * self.quantity
+        self.lineitem_total = self.price * self.quantity
         super().save(*args, **kwargs)
 
     def __str__(self):
-        return f'SKU {self.product} on order {self.order.order_number}'
+        return f'SKU {self.product_id} on order {self.order.order_number}'
