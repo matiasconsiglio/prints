dafb4d163b0c5ee5d2cf625a8685ea7adb4101ae
matiasconsiglio <macastro1@uc.cl>
2022-09-03 09:57:15 +0000

    Securing views

--- products/views.py
+++ products/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from django.db.models import Q
 from django.http import JsonResponse
 from django.db.models.functions import Lower
@@ -116,6 +117,7 @@ def add_to_cart(request):
     
     return JsonResponse({'data': bag})
 
+@login_required
 def add_product(request):
     """ Add a product to the store """
     if request.method == 'POST':
@@ -136,9 +138,12 @@ def add_product(request):
 
     return render(request, template, context)
 
-
+@login_required
 def add_product_spec(request):
     """ Add product specs to the store """
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry, only store owners can do that.')
+        return redirect(reverse('home'))
     if request.method == 'POST':
         form = SpecForm(request.POST)
         if form.is_valid():
@@ -157,8 +162,12 @@ def add_product_spec(request):
 
     return render(request, template, context)
 
+@login_required
 def edit_product(request, product_id):
     """ Edit a product in the store """
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry, only store owners can do that.')
+        return redirect(reverse('home'))
     product = get_object_or_404(Product, pk=product_id)
     if request.method == 'POST':
         form = ProductForm(request.POST, request.FILES, instance=product)
@@ -179,9 +188,13 @@ def edit_product(request, product_id):
     }
 
     return render(request, template, context)
-
+    
+@login_required
 def delete_product(request, product_id):
     """ Delete a product from the store """
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry, only store owners can do that.')
+        return redirect(reverse('home'))
     product = get_object_or_404(Product, pk=product_id)
     product.delete()
     messages.success(request, 'Product deleted!')
