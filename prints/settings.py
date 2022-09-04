3a9d9a5a4d27482f5fc2c15b587fdd83d450f5fa
matiasconsiglio <macastro1@uc.cl>
2022-09-04 16:02:51 +0000

    Add cache

--- prints/settings.py
+++ prints/settings.py
@@ -170,6 +170,11 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 if 'USE_AWS' in os.environ:
+    # Cache control
+    AWS_S3_OBJECT_PARAMETERS = {
+        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+        'CacheControl': 'max-age=94608000',
+    }
     # Bucket Config
     AWS_STORAGE_BUCKET_NAME = 'prints-by-futuro-berg'
     AWS_S3_REGION_NAME = 'eu-north-1'
