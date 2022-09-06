6cb97de9c3670dd66bb8b6b313be4053e54354cb
matiasconsiglio <macastro1@uc.cl>
2022-09-06 08:27:04 +0000

    fix jquery

--- static/js/personal.js
+++ static/js/personal.js
@@ -40,7 +40,7 @@ $(document).ready(function(){
     var _size=$(".size").first().attr('product-size');
     var _price=$(".paper").first().attr('product-price');
     $('.size'+_size).show();
-    $('.paper'+_size).first().addClass('active p');
+    $('.paper'+_price).first().addClass('active p');
     $(".product-price-variation").text(_price);
 
 
