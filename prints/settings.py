43b49982356ffe2d953ae5fffa4c6d2069e1002f
matiasconsiglio <macastro1@uc.cl>
2023-05-10 15:02:26 +0000

    heroku

--- prints/settings.py
+++ prints/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', 'ot0shejuv^bjmd%6$#hth^n_1y*4ef%o^^2k@+s0vaih!h(b7d')
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEPLOYMENT' in os.environ
